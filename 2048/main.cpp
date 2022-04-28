4c3434ede4507f3838873d3ccc03f73e11716327
Guillaume Longrais <guillaume.longrais@orange.fr>
2022-04-28 14:59:18 +0200

    implemented moveGrid()

--- 2048/main.cpp
+++ 2048/main.cpp
@@ -44,6 +44,7 @@ int** reverseTranspose(int** matrix, int size){
 input_info readFile(string filename){
     int** grid;
     grid = new int*[4];
+    int direction = 0;
     string line;
     ifstream input(filename);
     if(input.is_open()){
@@ -51,9 +52,12 @@ input_info readFile(string filename){
             getline(input, line);
             grid[i] = stringToTable(line);
         }
+        getline(input, line);
+        direction = stoi(line);
+
     }
     input.close();
-    input_info result = {grid, 3};
+    input_info result = {grid, direction};
     return result;
 }
 
@@ -64,18 +68,34 @@ int** moveGrid(int** grid, int direction){
     }
 
     for(int i = 0; i < 4; i++){
-        for(int j = 0; j < 4; j++){
-            printf("%i,", grid[i][j]);
+        for(int j = 0; j < 3; j++){
+            if(result[i][j]==0){
+                int tmp = j+1;
+                while(tmp < 3 && result[i][tmp] == 0){
+                    tmp++;
+                }
+                result[i][j] = result[i][tmp];
+                result[i][tmp] = 0;
+            }
         }
-        printf("\n");
-    }
-    printf("\n");
 
-    for(int i = 0; i < 4; i++){
-        for(int j = 0; j < 4; j++){
-            printf("%i,", result[i][j]);
+        for(int k = 0; k < 3; k++){
+            if(result[i][k]==result[i][k+1]){
+                result[i][k] *= 2;
+                result[i][k+1] = 0;
+            }
         }
-        printf("\n");
+
+        for(int l = 0; l < 3; l++){
+            if(result[i][l]==0){
+                int tmp = l+1;
+                while(tmp < 3 && result[i][tmp] == 0){
+                    tmp++;
+                }
+                result[i][l] = result[i][tmp];
+                result[i][tmp] = 0;
+            }
+        }
     }
 
     for(int i = 0; i < 4 - direction; i++){
@@ -85,9 +105,16 @@ int** moveGrid(int** grid, int direction){
     return result;
 }
 
-int main() {
-  input_info inputs = readFile("./Inputs/input_4.txt");
-  moveGrid(inputs.grid, inputs.direction);
+int main(int argc,  char **argv) {
+    input_info inputs = readFile(argv[1]);
+    int** result = moveGrid(inputs.grid, inputs.direction);
+
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            printf("%i ", result[i][j]);
+        }
+        printf("\n");
+    }
 
   return 0;
 }
