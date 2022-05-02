7ac5b084d901bc3b389d2e92846787a7fc681918
Guillaume Longrais <guillaume.longrais@orange.fr>
2022-05-02 14:53:25 +0200

    reading inputs

--- 10_kinds_of_people/main.cpp
+++ 10_kinds_of_people/main.cpp
@@ -3,13 +3,48 @@
 
 using namespace std;
 
-void readInput(int& grid, int& queries){
+struct query
+{
+    int r1;
+    int c1;
+    int r2;
+    int c2;
+};
+
+
+void readInput(char**& grid, query*& queries){
+    int x = 0;
+    int y = 0;
+
+    cin >> x >> y;
+
+    grid = new char* [x];
+
+    for(int i = 0; i < x; i ++){
+        grid[i] = new char[y];
+        cin >> grid[i];
+    }
+
+    int nb_queries = 0;
+
+    cin >> nb_queries;
+
+    queries =  new query[nb_queries];
+
+    for(int j = 0; j < nb_queries; j++){
+        int r1, c1, r2, c2;
+        cin >> r1 >> c1 >> r2 >> c2;
+        queries[j].r1 = r1;
+        queries[j].c1 = c1;
+        queries[j].r2 = r2;
+        queries[j].c2 = c2;
+    }
 
 }
 
 int main(){
-    int** grid;
-    int* queries;
+    char** grid;
+    query* queries;
 
     readInput(grid, queries);
 
