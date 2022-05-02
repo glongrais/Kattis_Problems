9fc69cb59bec218531af3d7b9e846228375c7c31
Guillaume Longrais <guillaume.longrais@orange.fr>
2022-05-02 13:15:25 +0200

    init file

---
+++ 10_kinds_of_people/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void readInput(int& grid, int& queries){
+
+}
+
+int main(){
+    int** grid;
+    int* queries;
+
+    readInput(grid, queries);
+
+}
